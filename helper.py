04d82afd35f2384b7b18d7d7abf2b5674b47c848
rebahozkoc <rebahozkoc@hotmail.com>
2019-10-25 16:40:24 +0300

    "update a open_libreoffice"

--- helper.py
+++ helper.py
@@ -1,10 +1,15 @@
 import socket
-import uno
-import os
 from sys import platform
-import random
+import os
+import shutil
 import subprocess
+import tempfile
 import time
+import uno
+import uuid
+
+from com.sun.star.connection import NoConnectException
+
 
 # get the uno component context from the PyUNO runtime
 
@@ -12,36 +17,79 @@ import time
 # create the UnoUrlResolver
 
 # connect to the running office
-#ctx = resolver.resolve( "uno:socket,host=localhost,port=2002;urp;StarOffice.ComponentContext" )
-#smgr = ctx.ServiceManager
+# ctx = resolver.resolve( "uno:socket,host=localhost,port=2002;urp;StarOffice.ComponentContext" )
+# smgr = ctx.ServiceManager
 
 # get the central desktop object
-#desktop = smgr.createInstanceWithContext( "com.sun.star.frame.Desktop",ctx)
+# desktop = smgr.createInstanceWithContext( "com.sun.star.frame.Desktop",ctx)
 
 # access the current document/component module
-#model = desktop.getCurrentComponent()
+# model = desktop.getCurrentComponent()
 
 # access the active sheet
-#active_sheet = model.CurrentController.ActiveSheet
+# active_sheet = model.CurrentController.ActiveSheet
 
 
-#def start_libreoffice():
+# def start_libreoffice():
 #    subprocess.Popen("soffice --calc --accept=\"socket,host=localhost,port=2002;urp;StarOffice.ServiceManager\"", shell=True)
 #    time.sleep(1)
 
 def start_libreoffice():
-    print("rehrefg")
+    sofficePath = '/usr/bin/soffice'
+    tempDir = tempfile.mkdtemp()
+    # Restore cached profile if available
+    userProfile = tempDir + '/profile'
+    cacheDir = os.getenv('XDG_CACHE_DIR', os.environ['HOME'] + '/.cache') + '/lo_profile_cache'
+    if os.path.isdir(cacheDir):
+        shutil.copytree(cacheDir, userProfile)
+        profileCached = True
+    else:
+        os.mkdir(userProfile)
+        profileCached = False
+    # Launch the LibreOffice server
+    pipeName = uuid.uuid4().hex
+    args = [
+        sofficePath,
+        '-env:UserInstallation=file://' + userProfile,
+        '--pidfile=' + tempDir + '/soffice.pid',
+        '--accept=pipe,name=' + pipeName + ';urp;',
+        '--norestore',
+        '--invisible']
+    sofficeEnvironment = os.environ
+    sofficeEnvironment['TMPDIR'] = tempDir
+    proc = subprocess.Popen(args, env=sofficeEnvironment, preexec_fn=os.setsid)
+    # Open connection to server
+    for i in range(100):
+        try:
+            localContext = uno.getComponentContext()
+            resolver = localContext.ServiceManager.createInstanceWithContext("com.sun.star.bridge.UnoUrlResolver",
+                                                                             localContext)
+            context = resolver.resolve("uno:pipe,name=%s;urp;StarOffice.ComponentContext" %
+                                       pipeName)
+            break
+        except NoConnectException:
+            time.sleep(0.1)
+            if i == 99:
+                raise
+    # Cache profile if required
+    if not profileCached:
+        shutil.copytree(userProfile, cacheDir)
+
+    desktop = context.ServiceManager.createInstanceWithContext("com.sun.star.frame.Desktop",context)
+    return desktop.loadComponentFromURL('private:factory/swriter', 'blank', 0, ())
+
+
 
 
 def connect_to_libreoffice():
     localContext = uno.getComponentContext()
-    resolver = localContext.ServiceManager.createInstanceWithContext("com.sun.star.bridge.UnoUrlResolver", localContext )
-    return resolver.resolve( "uno:socket,host=localhost,port=2002;urp;StarOffice.ComponentContext")
+    resolver = localContext.ServiceManager.createInstanceWithContext("com.sun.star.bridge.UnoUrlResolver", localContext)
+    return resolver.resolve("uno:socket,host=localhost,port=2002;urp;StarOffice.ComponentContext")
 
 
 def get_current_doc(ctx1):
     smgr = ctx1.ServiceManager
-    desktop = smgr.createInstanceWithContext( "com.sun.star.frame.Desktop",ctx1)
+    desktop = smgr.createInstanceWithContext("com.sun.star.frame.Desktop", ctx1)
     return desktop.getCurrentComponent()
 
 
@@ -49,14 +97,14 @@ def get_active_sheet(model1):
     return model1.CurrentController.ActiveSheet
 
 
-#active_sheet = get_active_sheet(get_current_doc(connect_to_libreoffice()))
+# active_sheet = get_active_sheet(get_current_doc(connect_to_libreoffice()))
 
 
 def get_cell_from_sheet(cell_name, sheet):
     return sheet.getCellRangeByName(cell_name)
 
 
-def set_cell_text(cell1,text):
+def set_cell_text(cell1, text):
     cell1.String = text
 
 
@@ -67,54 +115,57 @@ def get_cell_text(cell):
 def address_spliter(string):
     """Assumes string consist with letters and numbers, respectively.
      :returns a tuple whose first element is letters second is numbers"""
-    digits = ['0','1','2','3','4','5','6','7','8','9']
+    digits = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
     number = ""
-    for i in range(len(string)-1,-1,-1):
+    for i in range(len(string) - 1, -1, -1):
         if string[i] in digits:
             number = string[i] + number
         else:
-            return string[0:i+1], int(number)
+            return string[0:i + 1], int(number)
 
-def base26_to_decimal(start,end):
+
+def base26_to_decimal(start, end):
     """A supplementary function for field_determine"""
     base_number = 1
     end_value = 0
     start_value = 0
     while len(end) != 0:
-        end_value += (ord(end[-1])-64)*base_number
+        end_value += (ord(end[-1]) - 64) * base_number
         base_number *= 26
         end = end[:-1]
     base_number = 1
     while len(start) != 0:
-        start_value += (ord(start[-1])-64)*base_number
+        start_value += (ord(start[-1]) - 64) * base_number
         base_number *= 26
         start = start[:-1]
     return start_value, end_value
 
+
 def base26(x):
     """A supplementary function for field_determine"""
     number = ""
-    while x>26:
-        number = chr(x%26+64) + number
-        x = x//26
-    return chr(x+64) + number
+    while x > 26:
+        number = chr(x % 26 + 64) + number
+        x = x // 26
+    return chr(x + 64) + number
 
-def field_determine(starting,ending):
+
+def field_determine(starting, ending):
     """Assumes starting and ending are adresses of cells
     :returns cells' addresses between starting and ending"""
-    start,end = address_spliter(starting),address_spliter(ending)
-    decimal_value = base26_to_decimal(start[0],end[0])
+    start, end = address_spliter(starting), address_spliter(ending)
+    decimal_value = base26_to_decimal(start[0], end[0])
     address_list = []
-    for i in range(end[1]-start[1]+1):
-        for j in range(decimal_value[0], decimal_value[1]+1):
-            address_list.append((base26(j))+str(start[1]+i))
+    for i in range(end[1] - start[1] + 1):
+        for j in range(decimal_value[0], decimal_value[1] + 1):
+            address_list.append((base26(j)) + str(start[1] + i))
     return address_list
 
 
-def str_exists_in_cells(start_addr,end_addr, search_term):
+def str_exists_in_cells(start_addr, end_addr, search_term):
     """Assumes starting and ending are adresses of cells, and search_term is a string
     :returns True if search_term exits between start_addr and end_addr, false otherwise """
-    scope = field_determine(start_addr,end_addr)
+    scope = field_determine(start_addr, end_addr)
     for i in scope:
         inner_cell = active_sheet.getCellRangeByName(i)
         if inner_cell.String == search_term:
@@ -122,10 +173,10 @@ def str_exists_in_cells(start_addr,end_addr, search_term):
     return False
 
 
-def value_exists_in_cells(start_addr,end_addr, value):
+def value_exists_in_cells(start_addr, end_addr, value):
     """Assumes starting and ending are adresses of cells, and search_term is a value
     :returns True if search_term exits between start_addr and end_addr, false otherwise """
-    scope = field_determine(start_addr,end_addr)
+    scope = field_determine(start_addr, end_addr)
     for i in scope:
         inner_cell = active_sheet.getCellRangeByName(i)
         if inner_cell.Value == value:
@@ -133,10 +184,10 @@ def value_exists_in_cells(start_addr,end_addr, value):
     return False
 
 
-def search_str_in_cells(start_addr,end_addr, search_term):
+def search_str_in_cells(start_addr, end_addr, search_term):
     """Assumes starting and ending are adresses of cells, and search_term is a string
     :returns first occurrence of search_term's address if search_term exits between start_addr and end_addr, false otherwise """
-    scope = field_determine(start_addr,end_addr)
+    scope = field_determine(start_addr, end_addr)
     for i in scope:
         inner_cell = active_sheet.getCellRangeByName(i)
         if inner_cell.String == search_term:
@@ -144,10 +195,10 @@ def search_str_in_cells(start_addr,end_addr, search_term):
     return False
 
 
-def search_value_in_cells(start_addr,end_addr, value):
+def search_value_in_cells(start_addr, end_addr, value):
     """Assumes starting and ending are adresses of cells, and search_term is a value
     :returns first occurrence of search_term's address if search_term exits between start_addr and end_addr, false otherwise """
-    scope = field_determine(start_addr,end_addr)
+    scope = field_determine(start_addr, end_addr)
     for i in scope:
         inner_cell = active_sheet.getCellRangeByName(i)
         if inner_cell.Value == value:
@@ -162,7 +213,7 @@ def get_cell_text_with_addr(cell_addr):
     return inner_cell.String
 
 
-def set_cell_text_with_addr(cell_addr,new_str):
+def set_cell_text_with_addr(cell_addr, new_str):
     """Assumes cell_addr is an address of a cell and new_str is a string
     sets cell_addr to new_str """
     inner_cell = active_sheet.getCellRangeByName(cell_addr)
