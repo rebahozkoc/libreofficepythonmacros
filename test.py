8f885ccfedffe9f5c8fc85cd41a6cb1e05239a35
Rebah Özkoç <rebahozkoc@hotmail.com>
2019-11-03 22:40:27 +0300

    Writer testing function is completed.

--- test.py
+++ test.py
@@ -2,105 +2,9 @@ import helper
 import time
 
 
-def test_start_libreoffice():
-    try:
-        desktop = helper.start_libreoffice()
-        print("LibreOffice is started.")
-        assert desktop is not None
-        return desktop
-    except:
-        print("ERROR: LibreOffice could not be started.")
-        return False
-
-
-
-
-
-# def test_writer_functions(desktop):
-#     try:
-#         helper.writer_text_replace_all(doc, "office", "ofis", case_sensitive=False, whole_words=False)
-#         print("All 'office' words are changed with 'ofis")
-#         assert helper.writer_text_find(doc,"ofis")
-#     except:
-#         print("ERROR: Function: write_text_replace_all or writer_text_find SECTION: 1 ")
-#         error_counter += 1
-#     try:
-#         helper.writer_text_replace_all(doc, "office", "ofis", case_sensitive=False, whole_words=False)
-#         print("All 'office' words are changed with 'ofis', indeed.")
-#         assert helper.writer_text_find(doc,"ofis")
-#     except:
-#         print("ERROR: Function: write_text_replace_all or writer_text_find SECTION: 2 ")
-#         error_counter += 1
-#     try:
-#         helper.writer_text_replace_all(doc, "fork", "catal", case_sensitive=False, whole_words=True)
-#         print("All 'fork' whole words are changed with 'catal'.")
-#         assert not helper.writer_text_find(doc,"catal")
-#     except:
-#         print("ERROR: Function: write_text_replace_all or writer_text_find SECTION: 3 ")
-#         error_counter += 1
-#     try:
-#         helper.writer_text_replace_all(doc, "starofis", "yıldızofis", case_sensitive=True, whole_words=False)
-#         print("All 'starofis' words with lower case letter are changed with 'yıldızofis'.")
-#         assert not helper.writer_text_find(doc, "yıldızofis")
-#         assert helper.writer_text_find(doc, "Starofis")
-#     except:
-#         print("ERROR: Function: write_text_replace_all or writer_text_find SECTION: 4 ")
-#         error_counter += 1
-#     try:
-#         print("Test of writer_text_insert(doc, content) is starting")
-#         time.sleep(0.5)
-#         helper.writer_text_insert(doc, "TEST")
-#         print("Selected word changed with TEST")
-#         assert helper.writer_text_find(doc,"TEST")
-#     except:
-#         print("ERROR: Function: writer_text_insert SECTION: 1")
-#         error_counter += 1
-#     try:
-#         time.sleep(0.5)
-#         #text = doc.Text
-#         viewCursor = doc.getCurrentController()
-#         oVC = viewCursor.getViewCursor()
-#         oVC.goRight(1, False)
-#         helper.writer_text_insert(doc, "TEST")
-#         print("TEST is inserted.")
-#         assert helper.writer_text_find(doc,"TEST.TEST")
-#     except:
-#         print("ERROR: Function: writer_text_insert SECTION: 2")
-#         error_counter += 1
-#     try:
-#         time.sleep(0.5)
-#         helper.writer_image_insert_from_file(doc, "/home/rebahlinux/libreofficepythonmacros/image.jpg")
-#         print("An image imported.")
-#     except:
-#         print("ERROR: Function: writer_image_insert_from_file SECTION: 1")
-#         error_counter += 1
-#     try:
-#         time.sleep(0.5)
-#         helper.writer_image_insert_from_file(doc, "/home/rebahlinux/libreofficepythonmacros/image.jpg")
-#         print("Another image imported.")
-#     except:
-#         print("ERROR: Function: writer_image_insert_from_file SECTION: 2")
-#         error_counter += 1
-#     try:
-#         time.sleep(0.5)
-#         doc.close(False)
-#         print("End of the Writer testing.")
-#     except:
-#         print("ERROR: Writer could not be closed.")
-#         error_counter += 1
-#     else:
-#         print("test_writer_functions is completed with", error_counter,"error(s).\n\n\n")
-#         return True
-
-
-
-
-
-
-
 def standard_testing(function_name, expected_result, *args):
     """Assumes function_name is a function
-    expected_result ==  3 ==> next argument becomes standart_testing's argument and given function's return is compared with this argument.
+    expected_result ==  3 ==> next argument becomes standard_testing's argument and given function's return is compared with this argument.
     expected_result ==  2 ==> expected result is True,
     expected_result ==  1 ==> expected result is value,
     expected_result ==  0 ==> expected result is False,
@@ -136,7 +40,29 @@ def standard_testing(function_name, expected_result, *args):
         return result
 
 
-desktop = test_start_libreoffice()
+def test_writer_functions(desktop):
+    doc = standard_testing(helper.open_doc, 1, desktop, "/home/rebahlinux/libreofficepythonmacros/text_example.odt")
+    standard_testing(helper.writer_text_replace_all, -1, doc, "office", "placeholder",False, False)
+    standard_testing(helper.writer_text_find, 2, doc,"placeholder")
+    standard_testing(helper.writer_text_replace_all, -1, doc, "office", "placeholder", False, False)
+    standard_testing(helper.writer_text_find, 2, doc,"placeholder")
+    standard_testing(helper.writer_text_replace_all,-1, doc, "fork", "spoon", False, True)
+    standard_testing(helper.writer_text_find, 0, doc, "spoon")
+    standard_testing(helper.writer_text_replace_all, -1, doc, "starplaceholder", "planetplaceholder", True, False)
+    standard_testing(helper.writer_text_find, 0, doc, "planetplaceholder")
+    standard_testing(helper.writer_text_find, 2, doc, "Starplaceholder")
+    standard_testing(helper.writer_text_insert, -1, doc, "TEST")
+    standard_testing(helper.writer_text_find, 2, doc,"TEST")
+    viewCursor = doc.getCurrentController()
+    oVC = viewCursor.getViewCursor()
+    oVC.goRight(1, False)
+    standard_testing(helper.writer_text_insert, -1, doc, "TEST")
+    standard_testing(helper.writer_text_find, 2, doc,"TEST.TEST")
+    standard_testing(helper.writer_image_insert_from_file, -1, doc, "/home/rebahlinux/libreofficepythonmacros/image.jpg")
+    standard_testing(helper.writer_image_insert_from_file, -1, doc, "/home/rebahlinux/libreofficepythonmacros/image.jpg")
+    doc.close(False)
+
+
 def test_open_new_files(desktop):
     writer = standard_testing(helper.new_writer_doc, 1, desktop)
     writer.close(False)
@@ -144,6 +70,8 @@ def test_open_new_files(desktop):
     impress.close(False)
     calc = standard_testing(helper.new_calc_doc, 1, desktop)
     calc.close(False)
+
+
 def test_open_existing_file(desktop):
     doc = standard_testing(helper.open_doc, 1, desktop, "/home/rebahlinux/libreofficepythonmacros/text_example.odt")
     doc.close(False)
@@ -175,15 +103,13 @@ def test_calc_functions(desktop):
     standard_testing(helper.calc_get_cell_text_with_addr, 3, "", "C20", active_sheet)
     standard_testing(helper.calc_set_cell_text_with_addr, -1, "K9", "K9 is a smart dog.", active_sheet)
     standard_testing(helper.calc_get_cell_text_with_addr, 3, "K9 is a smart dog.", "K9", active_sheet)
+    doc.close(False)
 
 
-
-
-#test_open_new_files(desktop)
-#test_open_existing_file(desktop)
-#test_calc_functions(desktop)
-
-
-#test_writer_functions(desktop)
+desktop = standard_testing(helper.start_libreoffice, 1)
+test_open_new_files(desktop)
+test_open_existing_file(desktop)
 test_calc_functions(desktop)
+test_writer_functions(desktop)
+
 print("out of program.")
